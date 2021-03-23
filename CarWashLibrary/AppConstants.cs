7ec5ae709cef43f6e6533a2d523578e69708d457
Deividt Gemeli <deividt@gmail.com>
2021-03-23 08:11:06 -0300

    App Constants

---
+++ CarWashLibrary/AppConstants.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="AppConstants.cs" company="Deividt Gemeli">
+// Copyright (c) Deividt Gemeli.
+// </copyright>
+// ----------------------------------------------------------------------
+
+namespace CarWashLibrary
+{
+    using System;
+
+    /// <summary>
+    /// Classe <see cref="AppConstants" />
+    /// </summary>
+    public class AppConstants
+    {
+        #region AWS
+        /// <summary>
+        /// Obtém Identity Pool Id
+        /// </summary>
+        public static string CognitoIdentityPoolId { get => Environment.GetEnvironmentVariable("COGNITO_IDENTITY_POOL_ID") ?? string.Empty; }
+        #endregion
+
+        #region Syncfusion
+        /// <summary>
+        /// Obtém SyncfusionLicense
+        /// </summary>
+        public static string SyncfusionLicense { get => Environment.GetEnvironmentVariable("SYNCFUSION_LICENSE") ?? string.Empty; }
+        #endregion
+    }
+}
